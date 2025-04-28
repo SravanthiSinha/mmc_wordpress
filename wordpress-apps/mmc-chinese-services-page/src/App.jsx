48d7e4636ac008b3973a180bfd96c282482e50b2
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-28 01:08:41 -0700

    added chinese services page

---
+++ wordpress-apps/mmc-chinese-services-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import ChineseServices from './pages/ChineseServices';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <ChineseServices />
+    </div>
+  );
+}
+
+export default App;
