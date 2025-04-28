6ba4ad017e08daa5d843eef2280986f0914f5935
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-28 16:10:30 -0700

    add services page

---
+++ wordpress-apps/mmc-join_our_teampage/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import JoinOurTeam from './pages/JoinOurTeam';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <JoinOurTeam />
+    </div>
+  );
+}
+
+export default App;
---
+++ wordpress-apps/mmc-services-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import Specialties from './pages/Specialties';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <Specialties />
+    </div>
+  );
+}
+
+export default App;
