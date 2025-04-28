de19bd89aeeab85d96ddfb49529a1b6abaa1190d
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-28 13:06:07 -0700

    added about_us page

---
+++ wordpress-apps/mmc-about_us-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import About from './pages/About';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <About />
+    </div>
+  );
+}
+
+export default App;
