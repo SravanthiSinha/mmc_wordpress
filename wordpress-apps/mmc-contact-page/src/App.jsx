789bd644bf0eb6fac157d581e8c21033d0d83a06
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-28 13:26:09 -0700

    added contact us page

---
+++ wordpress-apps/mmc-contact-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import Contact from './pages/Contact';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
