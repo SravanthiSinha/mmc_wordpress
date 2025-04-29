01ff78c7bde882c261cd0de74ab3353244629fa3
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-29 12:46:53 -0700

    added individual therapy page

---
+++ wordpress-apps/mmc-individual_therapy-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import IndividualTherapy from './pages/IndividualTherapy';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <IndividualTherapy />
+    </div>
+  );
+}
+
+export default App;
