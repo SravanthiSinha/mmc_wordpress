ef23993dba5d7b2a7ac00cd7938b0b2bed59ff13
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-29 12:57:19 -0700

    added couples therapy page

---
+++ wordpress-apps/mmc-couples_therapy-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import CouplesTherapy from './pages/CouplesTherapy';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <CouplesTherapy />
+    </div>
+  );
+}
+
+export default App;
