2b3fd3b0815c429ed44097ff85688b3d1a59b886
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-04-29 18:48:01 -0700

    added privacy policy page

---
+++ wordpress-apps/mmc-privacy_policy-page/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="wp-react-app">
+      <PrivacyPolicy />
+    </div>
+  );
+}
+
+export default App;
